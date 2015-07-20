2884f9617a4efeaac641fdee750f7371b4334a38
ayato1995 <ayato.k070206@outlook.com>
2015-07-20 23:13:51 +0900

    騎士の表示

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 enchant();
+
+window.onload = function() {
+	var core = new Core(320, 320);
+	core.preload("./img/chara5.png");
+	core.onload = function() {
+		var knight = new Sprite(32, 32);
+		knight.image = core.assets["./img/chara5.png"];
+		knight.x = 50;
+		knight.y = 50;
+		core.rootScene.addChild(knight);
+	}
+	core.start();
+};
